0779c6d73f51bb24317048f2a81dc4142c08895a
misopog <misopog@tutanota.com>
2023-03-13 15:03:40 +0100

    dwm cleanup babyyyy

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -44,11 +44,11 @@ static const unsigned int borderalpha = OPAQUE;
 #if __has_include(wal)
 #include wal
 #else
-static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#454545";
-static const char col_gray3[]       = "#999999";
-static const char col_gray4[]       = "#606060";
-static const char col_gray5[]       = "#000000";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_gray5[]       = "#005577";
 static const char *colors[][3]      = {
 
 /*               fg         bg         border   */
